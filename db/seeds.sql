bee9a8ac31f57fc0a910ce9665a6fdd25b2772ca
Marilyn Wadsworth <marilyn.wadsworth86@gmail.com>
2019-06-23 19:20:36 -0600

    added all files, not completly functioning

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1,'Classic', true);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2,'Cheeseburger', true);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3,'Bacon', true);
+
