bb8f441cd42f932c3acfc107e5094865e6178cf3
Marilyn Wadsworth <marilyn.wadsworth86@gmail.com>
2019-08-08 21:56:53 -0600

    adding heroku requirements

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ if (process.env.JAWSDB_URL) {
   connection = my.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
-    password: 'yourRootPassword',
-    database: 'burger_db'
+    user: 'zavl113lxtibt7ea',
+    password: 'izjosymcyw2nu4p0',
+    database: 'oa7uit682bo6ifdj'
   });
 };
 
